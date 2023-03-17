9691ffc68b525cf4a01e5073e4dd3aff8020d464
Hasumeki <107523780+Hasumeki@users.noreply.github.com>
2023-03-17 20:55:08 +0800

    ProjectileSkeletonUpdate
    Added destruction parameters

--- Assets/Scripts/Projectile1.cs
+++ Assets/Scripts/Projectile1.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Projectile1 : MonoBehaviour
 {
     public Rigidbody2D rb;
+    [SerializeField] float lifetime = 5.0f;
+    float life = 0;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -12,6 +14,14 @@ public class Projectile1 : MonoBehaviour
     }
     void Update()
     {
-      
+      life += Time.deltaTime;
+        if(life >= lifetime)
+        {
+            Destroy(gameObject, 1);
+        }
+    }
+    void OnCollisionEnter2D()
+    {
+        Destroy(gameObject);
     }
 }
